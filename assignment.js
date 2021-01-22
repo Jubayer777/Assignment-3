7a25b6dcf9b17805ae2bc1afb128ab65f6c5bf4c
Jubayer <jubayeralmahmud24@gmal.com>
2021-01-22 15:59:32 +0600

    2nd Submit

--- assignment.js
+++ assignment.js
@@ -1,4 +1,7 @@
 
+ https://github.com/Jubayer777/Assignment-3
+
+
 
 //Problem-1: Convert distance from Kilometer to meter.
 function kilometerToMeter(kilometer){
@@ -7,12 +10,14 @@ function kilometerToMeter(kilometer){
         return meter;             
     }
     else{
-        return 'Invalid input';   // Return Worning for invalid input like- negetive distance 
+        return 'Invalid input';   // Return Worning for invalid input like- negative distance 
     }                             // return worning for wrong input data types such as array,string etc.
 }
 var km=5;
-var result1=kilometerToMeter(km);  //
+var result1=kilometerToMeter(km);  //calling function KilometerToMeter() with parameter km
 console.log(result1);
+
+
 
 
 //Problem-2:Calculating budget for electronic devices like Watch,Phone and Laptop.
@@ -25,14 +30,16 @@ function budgetCalculator(watchQuantity,phoneQuantity,laptopQuantity){
         return totalBudget;                                            
     }
     else{
-        return 'Invalid Input';               //Return warning for invalid input like quantity of watch,phone or laptop can't be negetive.
+        return 'Invalid Input';               //Return warning for invalid input like quantity of watch,phone or laptop can't be negative.
     }                                        //return warning for wrong input data types such as array,string etc.
 }
 var watch=8;
 var phone=5;
 var laptop=2;
-var result2=budgetCalculator(watch,phone,laptop);
+var result2=budgetCalculator(watch,phone,laptop); //calling function budgetCalculator() with parameter watch,phone and laptop.
 console.log(result2);
+
+
 
 
 //Prblem-3: Calculate the cost for hotel room reservation. cost for 1-10 days=100/day, 11-20 days=80/day, 21-above=50/day.
@@ -61,13 +68,16 @@ function hotelCost(days){
 
     }
     else{
-        return 'Invalid Input';             //Return warning for invalid input like if number of days is negetive or zero.
+        return 'Invalid Input';             //Return warning for invalid input like if number of days is negative or zero.
     }                                       //return warning for wrong input data types such as array,string etc.
     
 }
 var numberOfDays=21;
-var result3=hotelCost(numberOfDays);
+var result3=hotelCost(numberOfDays);       //calling function hotelCost() with parameter numberOfDays.
 console.log(result3);
+
+
+
 
 
 
@@ -84,10 +94,10 @@ function megaFriend(nameList){
     if(nameList.length!=0 && stringCount==nameList.length){      // check whether the array have elements or not and checking stringCount is equal to the length of array or not.
                                                                   //  perform further operation if and only if the aray is not empty and all the element of array are string.
         var maximumLength=nameList[0].length;                    //initialize the maximumLength with the length of first element of array.
-        var longestName=nameList[0];                             // assume that 1st lemet is longest.
+        var longestName=nameList[0];                             // assume that 1st element is longest.
         for(var i=0;i<nameList.length;i++){
             if(nameList[i].length>maximumLength){
-                maximumLength=nameList[i].length;                //update the value of maximumLengthe if there is any elemnt that's length is larger than maximum.
+                maximumLength=nameList[i].length;                //update the value of maximumLength if there is any elemnt that's length is larger than maximumLength.
                 longestName=nameList[i];                         //update the longestName with new element that have long length.
             }
         }
@@ -98,5 +108,5 @@ function megaFriend(nameList){
     }
 }
 var friends=['Jubayer','Rashed','Abdullah'];
-var result4=megaFriend(friends);
+var result4=megaFriend(friends);                            //calling function megaFriend() with perameter of array friends.
 console.log(result4);
